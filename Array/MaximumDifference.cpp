61292ee7fc760fcda1e1dc1bbfddf1cd568e070c
Dheemanth-gowda <dheethu12@gmail.com>
2021-01-26 09:42:12 +0530

    added one two functiond for Maximum Difference

---
+++ Array/MaximumDifference.cpp
@@ -0,0 +1,61 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int MaximumElementsBothSide(vector<int> arr)
+{
+    int maxNum = arr[0];
+    int minNum = arr[0];
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        if (arr[i] > maxNum)
+        {
+            maxNum = arr[i];
+        }
+        if (arr[i] < minNum)
+        {
+            minNum = arr[i];
+        }
+    }
+    return abs(maxNum - minNum);
+}
+
+// ! In this the difference is calculated in both direction
+
+int MaximumElement(vector<int> arr)
+{
+    int n = arr.size();
+    int maxNUM = arr[n - 1];
+    int res = INT_MIN;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (arr[i] > maxNUM)
+        {
+            maxNUM = arr[i];
+        }
+        else
+        {
+            res = max(res, (maxNUM - arr[i]));
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements" << endl;
+    cin >> n;
+
+    vector<int> arr;
+    cout << "Please enter the elements:" << endl;
+    int key;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> key;
+        arr.push_back(key);
+    }
+    cout << MaximumElementsBothSide(arr) << endl;
+    cout << "OUTPUT:" << MaximumElement(arr) << endl;
+}
